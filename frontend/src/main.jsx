2adcc2a728b6964d25d409a193195f3c2d5f5010
unknown <rifkhanmuhammed17@gmail.com>
2024-08-11 15:07:03 +0530

    deleted some files

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,10 @@
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import AppProvider from "./Context/AppContext.jsx";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <AppProvider>
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
     <App />
-  </AppProvider>
-);
+  </StrictMode>,
+)
